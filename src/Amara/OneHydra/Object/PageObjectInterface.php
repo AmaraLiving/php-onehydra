4acca82f10ef54e784e57bf77cede4d57bd1a755
Vincenzo Trapani <vincenzo.trapani@amara.com>
2016-02-02 09:25:04 +0000

    New page object interface

---
+++ src/Amara/OneHydra/Object/PageObjectInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Vincenzo Trapani <vincenzo.trapani@amara.com>
+ */
+
+namespace Amara\OneHydra\Object;
+
+/**
+ * Interface PageObjectInterface
+ * @package Amara\OneHydra\Object
+ */
+interface PageObjectInterface {
+}
